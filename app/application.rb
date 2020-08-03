bd82b16a09ad04f4f658ef28e7d4d500fa1dadcb
Kiri Um <kirirotha@gmail.com>
2020-08-03 16:55:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,8 @@
 class Application
 
     resp = Rack::Response.new
-
-    if Time.now < 12
+    t = Time.now
+    if t.hour < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!!"
